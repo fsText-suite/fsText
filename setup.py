e0c754ad78117fe28d87d51e15235389956f5a31
Mael Fabien <mael.fabien@anasen.com>
2019-09-13 10:59:34 +0200

    Deploy*

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
       name = 'fsText',         # How you named your package folder (MyLib)
       packages = ['fsText'],   # Chose the same as "name"
-      version = '0.1',      # Start with a small number and increase it with every change you make
+      version = '0.2',      # Start with a small number and increase it with every change you make
       license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
       description = 'Few Shot Text Classification',   # Give a short description about your library
       author = 'André, Matyas, Maël',                   # Type in your name
