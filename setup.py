e19ca5774307862abf324c39b5b4bafab7e7d1f5
Mael Fabien <mael.fabien@anasen.com>
2019-09-14 08:02:43 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
       name = 'fsText',         # How you named your package folder (MyLib)
       packages = ['fsText'],   # Chose the same as "name"
-      version = '0.3',      # Start with a small number and increase it with every change you make
+      version = '0.4',      # Start with a small number and increase it with every change you make
       license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
       description = 'Few Shot Text Classification',   # Give a short description about your library
       author = 'André, Matyas, Maël',                   # Type in your name
       author_email = 'mael.fabien@gmail.com',      # Type in your E-Mail
       url = 'https://github.com/maelfabien/fsText',   # Provide either the link to your github or to your website
-      download_url = 'https://github.com/maelfabien/fsText/archive/v_03.tar.gz',    # I explain this later on
+      download_url = 'https://github.com/maelfabien/fsText/archive/v_04.tar.gz',    # I explain this later on
       keywords = ['Few', 'Shot', 'Text', 'Classification'],   # Keywords that define your package best
       install_requires=[
                         'gensim',
